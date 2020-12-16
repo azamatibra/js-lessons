608228b17dc8ea671f86dcef0fb835372a57d009
Musa <black.nobody.anb@gmail.com>
2020-12-16 11:08:39 +0600

    Added lesson_1

---
+++ lesson_01/js/main.js
@@ -0,0 +1,80 @@
+// 1
+// let question = prompt('Сколько будет 2 + 2 / 2?')
+// if(question === '3' || question === 'three') {
+//   alert("Верно")
+// }else {
+//   alert("Неверно")
+// }
+// 2
+// let number = parseInt(prompt('Введите число: '));
+// if (number > 1 && number < 3){
+//   alert("Куда поведут туда и идите))")
+// } else if (number > 3 && number < 10) {
+//   alert("Проситесь на детскую площадку")
+// } else {
+//   alert("Домой")
+// }
+// 3
+// let speed = parseInt(prompt('Введите числа от 1 до 9'))
+// switch (speed) {
+//   case 1:
+//   case 2:
+//   case 3:
+//     alert('Низкая скорость')
+//     break;
+//   case 4:
+//   case 5:
+//   case 6:
+//     alert("Средняя скорость")
+//     break;
+//   case 7:
+//   case 8:
+//   case 9:
+//     alert("Высокая скорость")
+//     break;
+//   default:
+//     alert('Неправильные данные')
+//     break;
+// }
+// Variant 2
+// let speed = parseInt(prompt('Введите числа от 1 до 9'))
+// if (speed >= 1 && speed <= 3){
+//   alert('Низкая скорость')
+// } else if (speed >= 4 && speed <= 6){
+//   alert("Средняя скорость")
+// } else if (speed >= 7 && speed <= 9){
+//   alert("Высокая скорость")
+// } else {
+//   alert("Неправильные данные")
+// }
+// 4
+// let mortar = parseInt(prompt("Введи число от 28 до 30"));
+// if (mortar <= 0) {
+//   alert("НЕ БЕЙ ПО СВОИМ");
+// } else if (mortar > 0 && mortar <= 28) {
+//   alert("НЕДОЛЕТ");
+// } else if (mortar > 28 && mortar < 30) {
+//   alert("ПОПАЛ");
+// } else if (mortar >= 30) {
+//   alert("ПЕРЕЛЕТ");
+// } else {
+//   alert("ОШИБКА")
+// }
+// 5
+// let mark = parseInt(prompt("Узнай оценку: "));
+// switch (mark) {
+//   case 2:
+//     alert("Неудовлетворительно");
+//     break;
+//   case 3:
+//     alert("Удовлетворительно")
+//     break;
+//   case 4:
+//     alert("Хорошо")
+//     break;
+//   case 5:
+//     alert("Отлично")
+//     break;
+//   default:
+//     alert("Введите правильную оценку");
+// }
