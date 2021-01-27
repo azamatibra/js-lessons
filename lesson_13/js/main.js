6bdbe2e4f1e25aeb87f82037725ce122910198bc
Musa <black.nobody.anb@gmail.com>
2021-01-27 13:22:23 +0600

    Edited lesson_13

--- lesson_13/js/main.js
+++ lesson_13/js/main.js
@@ -23,12 +23,14 @@
 
 // 1
 // $(function () {
-//   let dataText = $(".search__input");
-//   (search = $(".search")),
-//     (country = $("#name")),
-//     (capital = $("#capital")),
-//     (region = $("#region")),
-//     (subRegion = $("#subregion"));
+//   let dataText = $(".search__input"),
+//     search = $(".search"),
+//     country = $("#name"),
+//     capital = $("#capital"),
+//     region = $("#region"),
+//     subRegion = $("#subregion");
+
+
 //   let countryName, countryCapital, countryRegion, countrySubRegion;
 //   search.on("submit", function (e) {
 //     e.preventDefault();
@@ -63,55 +65,43 @@
 
 $(function () {
   $("table").addSortWidget();
-  let countryNames = [];
   let search = $(".search");
-
   search.on("submit", function (e) {
     e.preventDefault();
     let searchText = $(".search__input").val();
-    for (let i = 0; i < countryNames.length; i++) {
-      if (searchText == countryNames[i]) {
-        let name = document.querySelectorAll(".table__name");
-        for (let i = 0; i < name.length; i++) {
-          country = name[i].textContent;
-          if (country == searchText) {
-            $("tbody").prepend($(name[i].closest("tr")));
-          }
-        }
+    let name = $(".table__name");
+    for (let i = 0; i < name.length; i++) {
+      let countryName = $(name[i]).text();
+      if (searchText == countryName) {
+        $("tbody").prepend($(name[i].closest("tr")));
       }
     }
   });
-
   $.ajax({
     type: "GET",
     url: "https://restcountries.eu/rest/v2/all",
-    success: function (response) {
-      for (let i = 0; i < 10; i++) {
-        let box = $("<tr>");
-        let td = $("<td>");
-        let number = $("<td>").text(i + 1);
-        let code = $("<td>").text(response[i].alpha2Code);
-        let flag = $('<img class="table__flag-img" src="">').attr(
-          "src",
-          response[i].flag
-        );
-        let name = $('<td class="table__name"></td>').text(response[i].name);
-        let capital = $('<td class="table__capital"></td>').text(
-          response[i].capital
-        );
-        let population = $('<td class="table__population"></td>').text(
-          response[i].population
-        );
-        $("tbody").append(box);
-        box.append(number);
-        box.append(code);
-        box.append(td);
-        td.append(flag).addClass("table__flag");
-        box.append(name);
-        box.append(capital);
-        box.append(population);
-        countryNames.push(name.text());
-      }
-    },
-  });
+  }).done(printCountry);
+  function printCountry(response) {
+    for (let i = 0; i < response.length; i++) {
+      let box = $("<tr>");
+      let td = $("<td>");
+      let number = $("<td>").text(i + 1);
+      let code = $("<td>").text(response[i].alpha2Code);
+      let flag = $('<img class="table__flag-img" src="">').attr(
+        "src",
+        response[i].flag
+      );
+      let name = $('<td class="table__name"></td>').text(response[i].name);
+      let capital = $('<td class="table__capital"></td>').text(
+        response[i].capital
+      );
+      let population = $('<td class="table__population"></td>').text(
+        response[i].population
+      );
+      $("tbody").append(box);
+      box.append(number, code, td);
+      td.append(flag).addClass("table__flag");
+      box.append(name, capital, population);
+    }
+  }
 });
