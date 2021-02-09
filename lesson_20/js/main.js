26b51afce4c7ec111b75865012a0a28ac9be97fb
Musa <black.nobody.anb@gmail.com>
2021-02-09 17:28:20 +0600

    version 0.1

--- lesson_20/js/main.js
+++ lesson_20/js/main.js
@@ -2,6 +2,15 @@ $(() => {
   let btn = $(".form-button");
   let form = $(".form");
   let input;
+  let request = function (url) {
+    return new Promise((resolve, reject) => {
+      $.ajax({
+        url,
+        success: resolve,
+        error: reject,
+      });
+    });
+  };
   form.on("submit", function (e) {
     let wrap = $(".country__wrap");
     wrap.html("");
@@ -9,42 +18,46 @@ $(() => {
     input = $("#form-input").val();
     this.reset();
 
-    $.ajax({
-      url: "https://restcountries.eu/rest/v2/name/" + input,
-      success: showCountry,
-    });
-    function showCountry(response) {
+    if (input != "") {
+      request("https://restcountries.eu/rest/v2/name/" + input)
+        .then(findBorder)
+        .then(showCountry)
+        .catch((err) => console.log(err));
+    } else {
+      console.log("Заполните!");
+    }
+    function showCode(response) {
       let title = $(".country__title").addClass("country__title-custom");
       title.text(input);
       let border = response[0].borders;
-      let flagUrl = response[0].flag;
-      let flag = $(`<img src="${flagUrl}" class="country__flag">`);
       for (let i = 0; i < border.length; i++) {
-        let countryElement = $('<div class="country__border"></div>');
+        let countryElement = $('<div href="#" class="country__border bg-info"></div>');
         let countryName = $('<span class="country__name"></span>');
         wrap.append(countryElement);
         countryElement.append(countryName.text(border[i]));
       }
     }
-    findCountry();
-    function findCountry() {
-      setTimeout(() => {
-        let names = $(".country__name");
-        let borders = $('.country__border') 
-        for (let i = 0; i < names.length; i++) {
-          $.ajax({
-            url:
-              "https://restcountries.eu/rest/v2/alpha/" + $(names[i]).text(),
-            success: function (response) {
-              let flag = $(
-                `<img class="country__flag" src="${response.flag}">`
-              );
-              $(names[i]).text(response.name);
-              $(borders[i]).append(flag);
-            },
-          });
-        }
-      }, 2000);
+
+    function findBorder(response) {
+      let country = response[0];
+      showCode(response);
+      let borders = [];
+      for (let border of country.borders) {
+        borders.push(
+          request("https://restcountries.eu/rest/v2/alpha/" + border)
+        );
+      }
+      return Promise.all(borders);
+    }
+
+    function showCountry(response) {
+      let names = $(".country__name");
+      let borders = $(".country__border");
+      for (let i = 0; i < names.length; i++) {
+        let flag = $(`<img class="country__flag" src="${response[i].flag}">`);
+        $(names[i]).text(response[i].name);
+        $(borders[i]).append(flag);
+      }
     }
   });
 });
