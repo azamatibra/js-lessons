35075c12bf5d79dc4d8c3dcc5dc1f8a00f1f0577
Musa <black.nobody.anb@gmail.com>
2021-02-16 17:04:22 +0600

    verion 0.1

--- lesson_22/js/main.js
+++ lesson_22/js/main.js
@@ -1,40 +1,64 @@
 $(() => {
   let baseUrl = "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=20";
   let pokemonData = $(".pokemon__data");
+  let pokemonFormSearch = $(".pokemon__form-search");
   let next = $(".pokemon__next");
   let prev = $(".pokemon__prev");
-  let request = function (url) {
-    return new Promise((resolve, reject) => {
-      $.ajax({
-        url,
-        success: resolve,
-        error: reject,
-      });
-    });
+  let pokemonPagePrev = $(".pokemon__page-prev");
+  let pokemonPageNext = $(".pokemon__page-next");
+  let pokemonState;
+  let pokemonCurrentPageNumber = $("#pokemon-current");
+  let currentPageNumber = 1;
+
+  pokemonFormSearch.on("submit", async function (e) {
+    let pokemonSearchInput = $(".pokemon__search");
+    let pokemonInfo = pokemonSearchInput.val();
+    let pokemonAllReq = await request(
+      "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=1118"
+    );
+    for (let i = 0; i < pokemonAllReq.results.length; i++) {
+      if (pokemonInfo == pokemonAllReq.results[i].name) {
+        pokemonData.html("");
+        createPokemon(await request(pokemonAllReq.results[i].url));
+      }
+    }
+    this.reset();
+  });
+
+  let request = async function (url) {
+    return (await fetch(url)).json();
   };
 
-  let pokemonState;
-  async function getPokemon() {
+  async function getPokemon(url) {
     pokemonData.html("");
-    let title = await request(baseUrl);
+    let title = await request(url);
     renderPokemon(title);
   }
 
-  getPokemon();
+  getPokemon(baseUrl);
   async function renderPokemon(pokemon) {
     pokemonState = pokemon;
     for (let i = 0; i < pokemon.results.length; i++) {
       pokemonReq = await request(pokemon.results[i].url);
-      let pokemonImgUrl;
-      let pokemonName = pokemonReq.name;
-      let pokemonType = [];
-      let pokemonGrowth = pokemonReq.height;
-      let pokemonWeight = pokemonReq.weight;
-      for (let i = 0; i < pokemonReq.types.length; i++) {
-        pokemonType.push(pokemonReq.types[i].type.name);
-      }
-      pokemonImgUrl = pokemonReq.sprites.front_default;
-      let pokemonBox = `
+      createPokemon(pokemonReq);
+    }
+  }
+
+  function createPokemon(pokemonReq) {
+    let pokemonImgUrl;
+    let pokemonName = pokemonReq.name;
+    let pokemonType = [];
+    let pokemonGrowth = pokemonReq.height;
+    let pokemonWeight = pokemonReq.weight;
+    for (let i = 0; i < pokemonReq.types.length; i++) {
+      pokemonType.push(pokemonReq.types[i].type.name);
+    }
+    pokemonImgUrl = pokemonReq.sprites.front_default;
+    if (pokemonReq.sprites.front_default == null) {
+      pokemonImgUrl =
+        pokemonReq.sprites.other["official-artwork"].front_default;
+    }
+    let pokemonBox = `
       <div class="pokemon__box">
         <div class="pokemon__img">
           <img
@@ -71,21 +95,42 @@ $(() => {
         </div>
       </div>
       `;
-      pokemonData.append(pokemonBox);
-    }
+    pokemonData.append(pokemonBox);
   }
 
   next.on("click", async function (e) {
     pokemonData.html("");
     let pokemonPage = await request(pokemonState.next);
-    console.log(pokemonPage);
     renderPokemon(pokemonPage);
+    currentPageNumber++;
+    pokemonCurrentPageNumber.text(currentPageNumber);
   });
   prev.on("click", async function (e) {
     if (pokemonState.previous != null) {
       pokemonData.html("");
       let pokemonPage = await request(pokemonState.previous);
       renderPokemon(pokemonPage);
+      currentPageNumber--;
+      pokemonCurrentPageNumber.text(currentPageNumber);
     }
+  });
+
+  pokemonPagePrev.on("click", async function (e) {
+    pokemonData.html("");
+    currentPageNumber = 1;
+    pokemonCurrentPageNumber.text(currentPageNumber);
+    let pokemonPage = await request(baseUrl);
+    renderPokemon(pokemonPage);
+  });
+  pokemonPageNext.on("click", async function (e) {
+    let pokemonLimit = 20;
+    let pokemonNumbers = pokemonState.count - pokemonLimit;
+    pokemonData.html("");
+    currentPageNumber = Math.round(pokemonNumbers / pokemonLimit);
+    pokemonCurrentPageNumber.text(currentPageNumber);
+    let pokemonPage = await request(
+      `https://pokeapi.co/api/v2/pokemon/?offset=${pokemonNumbers}&limit=${pokemonLimit}`
+    );
+    renderPokemon(pokemonPage);
   });
 });
